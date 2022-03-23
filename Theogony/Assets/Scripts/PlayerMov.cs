104894e130e63e3d6929f44cfa386f0dbb9e78cc
Bennacy <benjamimrodrigues.mg@gmail.com>
2022-03-23 16:38:04 +0000

    Movement Follows Camera Facing

---
+++ Theogony/Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float walkSpeed;
+    public float runSpeed;
+    private float moveSpeed;
+    public float turnTime;
+    public Camera cam;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float camRot = cam.transform.rotation.eulerAngles.y;
+        Vector3 camForward = Quaternion.Euler(0, camRot, 0) * Vector3.forward;
+
+        if(Input.GetKey(KeyCode.LeftShift)){
+            moveSpeed = runSpeed;
+        }else{
+            moveSpeed = walkSpeed;
+        }
+
+        Vector3 vel = Vector3.zero;
+        if(Input.GetKey(KeyCode.W)){
+            vel += (camForward * moveSpeed);
+        }
+        if(Input.GetKey(KeyCode.S)){
+            vel += (Quaternion.Euler(0, 180, 0) * camForward * moveSpeed);
+        }
+        if(Input.GetKey(KeyCode.D)){
+            vel += (Quaternion.Euler(0, 90, 0) * camForward * moveSpeed);
+        }
+        if(Input.GetKey(KeyCode.A)){
+            vel += (Quaternion.Euler(0, -90, 0) * camForward * moveSpeed);
+        }
+        rb.velocity = vel;
+        Vector3 movementDir = vel.normalized;
+        // Debug.Log(camForward);
+        if(movementDir != Vector3.zero){
+            float angle = Vector3.SignedAngle(Vector3.forward, movementDir, Vector3.up);
+            Debug.Log(angle);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, angle, 0), Time.deltaTime * turnTime);
+        }
+    }
+}
---
+++ Theogony/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(Input.GetKey(KeyCode.UpArrow)){
+
+        }
+        if(Input.GetKey(KeyCode.DownArrow)){
+            
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            Vector3 rot = transform.rotation.eulerAngles;
+            rot.y -= 10 * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(rot);
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){
+            Vector3 rot = transform.rotation.eulerAngles;
+            rot.y += 10 * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(rot);
+        }
+    }
+}
