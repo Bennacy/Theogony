417d2ad8991a0ee5d6178d683ade88782f9798ed
Bennacy <benjamimrodrigues.mg@gmail.com>
2022-03-30 13:54:37 +0100

    VascoStuff

---
+++ Assets/VascoStuff/PlayerManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Theogony
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        InputHandler inputHandler;
+
+        CameraHandler cameraHandler;
+        PlayerLocomotaion playerLocomotion;
+
+
+        private void Awake()
+        {
+            cameraHandler = CameraHandler.singleton;
+        }
+
+        void Start()
+        {
+            inputHandler = GetComponent<InputHandler>();
+            playerLocomotion = GetComponent<PlayerLocomotaion>();
+        }
+
+
+        void Update()
+        {
+            float delta = Time.deltaTime;
+            inputHandler.TickInput(delta);
+            playerLocomotion.HandleMovement(delta);
+           
+        }
+
+        private void FixedUpdate()
+        {
+            float delta = Time.fixedDeltaTime;
+
+            if (cameraHandler != null)
+            {
+                cameraHandler.FollowTarget(delta);
+                cameraHandler.HandleCameraRotation(delta, inputHandler.mouseX, inputHandler.mouseY);
+            }
+        }
+
+
+    }
+}
