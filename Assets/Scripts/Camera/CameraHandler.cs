c53a6e277936af2e40e600f15e7017823f170808
Bennacy <benjamimrodrigues.mg@gmail.com>
2022-04-06 15:59:56 +0100

    Mouse camera control improved

--- Assets/Scripts/Camera/CameraHandler.cs
+++ Assets/Scripts/Camera/CameraHandler.cs
@@ -13,6 +13,7 @@ namespace Theogony
         private Transform myTransform;
         private Vector3 cameraTransformPosition;
         private LayerMask ignoreLayers;
+        private PlayerInput playerInput;
 
         public static CameraHandler singleton;
 
@@ -28,8 +29,10 @@ namespace Theogony
         [Space]
 
         [Space]
-        public float targetAngle;
+        public float targetAngleX;
+        public float targetAngleY;
         private bool resetCam;
+        public float mouseSens;
         public float lookSpeed = 0.1f;
         public float followSpeed = 0.1f;
         public float pivotSpeed = 0.03f;
@@ -56,6 +59,8 @@ namespace Theogony
             myTransform = transform;
             defaultPosition = cameraTransform.localPosition.z;
             ignoreLayers = ~(1 << 8 | 1 << 9 | 1 << 10);
+            playerInput = player.gameObject.GetComponent<PlayerInput>();
+            lookAngle = targetAngleX = targetAngleY = 0;
         }
 
         public void FollowTarget(float delta)
@@ -67,23 +72,32 @@ namespace Theogony
 
         public void HandleCameraRotation(float delta)
         {
-            lookAngle += (mouseXInput * lookSpeed) / delta;
-            pivotAngle -= (mouseYInput * pivotSpeed) / delta;
-            pivotAngle = Mathf.Clamp(pivotAngle, minimumPivot, maximumPivot);
+            targetAngleX += (mouseXInput * lookSpeed) / delta;
+            if(targetAngleX >= 360){
+                targetAngleX -= 360;
+            }else if(targetAngleX <= 0){
+                targetAngleX += 360;
+            }
+            
+            Debug.Log(mouseYInput);
+            targetAngleY -= (mouseYInput * pivotSpeed) / delta;
+            targetAngleY = Mathf.Clamp(targetAngleY, minimumPivot, maximumPivot);
 
             Vector3 rotation = Vector3.zero;
             rotation.y = lookAngle;
             Quaternion targetRotation = Quaternion.Euler(rotation);
             myTransform.rotation = targetRotation;
 
+            if(lookAngle != targetAngleX){
+                lookAngle = Mathf.LerpAngle(lookAngle, targetAngleX, lookSpeed);
+            }
             rotation = Vector3.zero;
             rotation.x = pivotAngle;
 
-            // if(lookAngle != targetAngle){
-            //     lookAngle = Mathf.Lerp(lookAngle, targetAngle, lookSpeed);
-            // }else{
+            if(pivotAngle != targetAngleY){
+                pivotAngle = Mathf.LerpAngle(pivotAngle, targetAngleY, lookSpeed);
+            }
 
-            // }
 
             targetRotation = Quaternion.Euler(rotation);
             cameraPivotTransform.localRotation = targetRotation;
@@ -119,10 +133,10 @@ namespace Theogony
                 lockOnIndicator.transform.position = new Vector3(lockOnIndicator.transform.position.x, lockOnIndicator.transform.position.y + 1, lockOnIndicator.transform.position.z);
 
                 Vector3.Normalize(direction);
-                targetAngle = Vector3.SignedAngle(Vector3.forward, direction, Vector3.up);
+                targetAngleX = Vector3.SignedAngle(Vector3.forward, direction, Vector3.up);
                 
-                if(lookAngle != targetAngle){
-                    lookAngle = Mathf.Lerp(lookAngle, targetAngle, lookSpeed);
+                if(lookAngle != targetAngleX){
+                    lookAngle = Mathf.LerpAngle(lookAngle, targetAngleX, lookSpeed);
                 }
                 if(Vector3.Distance(player.transform.position, lockOnTarget.position) > lockOnRange){
                     lockOnTarget = null;
@@ -132,11 +146,25 @@ namespace Theogony
 
         public void MoveCamera(InputAction.CallbackContext context){
             Vector2 value = context.ReadValue<Vector2>();
+            value *= Time.deltaTime * mouseSens;
+            
             if(context.performed){
+                if(playerInput.currentControlScheme == "Keyboard"){
+                    // Debug.Log(value);
+                    Cursor.lockState = CursorLockMode.Locked;
+                }else{
+                    Cursor.lockState = CursorLockMode.None;
+                }
+
                 if(lockOnTarget == null){
                     mouseXInput = value.x;
                     mouseYInput = value.y;
                 }else{
+                    if(playerInput.currentControlScheme == "Keyboard"){
+                        float temp = value.x;
+                        value.x = value.y;
+                        value.y = temp;
+                    }
                     if(stoppedMove){
                         colliders = Physics.OverlapSphere(player.transform.position, lockOnRange, enemyLayer);
                         int index = 0;
@@ -190,8 +218,8 @@ namespace Theogony
                     lockOnTarget = colliders[0].transform;
                 }else{
                     Quaternion angle = player.rb.rotation;
-                    transform.rotation = angle;
-                    lookAngle = angle.eulerAngles.y;
+                    // transform.rotation = angle;
+                    targetAngleX = angle.eulerAngles.y;
                 }
             }
         }
