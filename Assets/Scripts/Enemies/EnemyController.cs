659a4ebda96c05f63deda134e152e50593111204
Bennacy <benjamimrodrigues.mg@gmail.com>
2022-04-16 10:48:32 +0100

    Enemies follow player

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -12,20 +12,37 @@ namespace Theogony{
         private GlobalInfo globalInfo;
         private Rigidbody rb;
         private ParticleSystem blood;
+        public LayerMask viewLayer;
+        public Collider[] inRange;
+        public float viewRange;
+        public float moveSpeed;
+        public Transform target;
 
         void Start()
         {
             globalInfo = GlobalInfo.GetGlobalInfo();
             blood = GetComponentInChildren<ParticleSystem>();
-            // rb = GetComponent<Rigidbody>();
+            rb = GetComponent<Rigidbody>();
             currHealth = maxHealth;
         }
 
         void Update()
         {
-            // rb.velocity = Vector3.back;
             if(currHealth <= 0){
                 Kill();
+            }
+
+            inRange = Physics.OverlapSphere(transform.position, viewRange, viewLayer);
+            if(inRange.Length != 0){
+                target = inRange[0].transform;
+            }else{
+                target = null;
+            }
+
+            if(target != null){
+                rb.velocity = GetDirection(transform.position, target.position) * moveSpeed;
+            }else{
+                rb.velocity = Vector3.zero;
             }
         }
 
@@ -41,11 +58,23 @@ namespace Theogony{
         void OnCollisionEnter(Collision collision){
             if(collision.gameObject.layer == 8){
                 DamageCollider enemyController = collision.gameObject.GetComponent<DamageCollider>();
-                Damage(globalInfo.str + 1);
+                Damage(10);
                 blood.transform.position = collision.contacts[0].point;
+                Vector3 direction = GetDirection(collision.transform.position, transform.position);
+                direction.y = 0;
+                rb.AddForce(direction * 10, ForceMode.Impulse);
                 blood.Play();
                 // Debug.Log(collision.contacts[0].point);
             }
+        }
+
+        private Vector3 GetDirection(Vector3 position1, Vector3 position2){
+            return Vector3.Normalize(position2 - position1);
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.DrawWireSphere(transform.position, viewRange);
         }
     }
 }
