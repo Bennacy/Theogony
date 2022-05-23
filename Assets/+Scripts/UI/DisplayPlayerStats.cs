385589f955f25a77e840055c357e7cd798eb65fd
Bennacy <benjamimrodrigues.mg@gmail.com>
2022-05-23 13:32:26 +0100

    Better display in loadout

--- Assets/+Scripts/UI/DisplayPlayerStats.cs
+++ Assets/+Scripts/UI/DisplayPlayerStats.cs
@@ -8,10 +8,19 @@ namespace Theogony{
     public class DisplayPlayerStats : MonoBehaviour
     {
         public TextMeshProUGUI statText;
+        public TextMeshProUGUI[] levelDisplays;
+        public TextMeshProUGUI healthText;
+        public TextMeshProUGUI staminaText;
+        public TextMeshProUGUI damageText;
         public GlobalInfo globalInfo;
+        private PlayerManager playerManager;
+        private PlayerInventory playerInventory;
+        private weaponItems weapon;
         void Start()
         {
             globalInfo = GlobalInfo.GetGlobalInfo();
+            playerManager = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>();
+            playerInventory = playerManager.GetComponent<PlayerInventory>();
         }
 
 
@@ -23,7 +32,28 @@ namespace Theogony{
             finalString += "\nStrength:\t\t" + (globalInfo.str+1).ToString();
             finalString += "\nDexterity:\t\t" + (globalInfo.dex+1).ToString();
 
-            statText.text = finalString;
+            for(int i = 0; i < levelDisplays.Length; i++){
+                switch(i){
+                    case 0:
+                        levelDisplays[i].text = "Vitality:\t\t" + (globalInfo.vit + 1);
+                        break;
+                    case 1:
+                        levelDisplays[i].text = "Endurance:\t\t" + (globalInfo.end + 1);
+                        break;
+                    case 2:
+                        levelDisplays[i].text = "Strength:\t\t" + (globalInfo.str + 1);
+                        break;
+                    case 3:
+                        levelDisplays[i].text = "Dexterity:\t\t" + (globalInfo.dex + 1);
+                        break;
+                }
+            }
+
+            weapon = playerInventory.rightWeapon;
+            healthText.text = "Max Health:\t" + Mathf.Round(playerManager.maxHealth);
+            staminaText.text = "Max Stamina:\t" + Mathf.Round(playerManager.maxStamina);
+            int currentDamage = Mathf.RoundToInt(weapon.CalculateDamage(globalInfo, false));
+            damageText.text = "Damage:\t" + currentDamage;
         }
     }
 }
