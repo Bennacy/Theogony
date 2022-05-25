72b473dc5d842ab3b33541169fa1b2ed337bfa97
Bennacy <benjamimrodrigues.mg@gmail.com>
2022-05-25 22:45:03 +0100

    Revert "Finalized dropdown"
    This reverts commit 78098a2007b4e5734df8c1ea999a83c6d5d63d66.

--- Assets/+Scripts/UI/CustomDropdown.cs
+++ Assets/+Scripts/UI/CustomDropdown.cs
@@ -14,10 +14,7 @@ namespace Theogony{
         public bool dropdownOpen;
         public GameObject optionHolder;
         public Button[] options;
-        public int currentOption;
-        private int previousIndex;
-        public Image arrowImage;
-        public Sprite[] arrowSprites;
+        public int optionIndex;
         public Button parentButton;
 
         void Start()
@@ -25,14 +22,13 @@ namespace Theogony{
             dropdownOpen = false;
             globalInfo = GlobalInfo.GetGlobalInfo();
             options = optionHolder.GetComponentsInChildren<Button>();
-            optionString = options[currentOption].gameObject.name;
+            optionString = options[optionIndex].gameObject.name;
             foreach (Button option in options){
                 option.GetComponentInChildren<TextMeshProUGUI>().text = option.gameObject.name;
             }
             optionText.text = optionString;
             uIController = GetComponentInParent<UIController>();
             parentButton.onClick.AddListener(delegate{ToggleDropdown();});
-            arrowImage.sprite = arrowSprites[0];
         }
 
         void Update()
@@ -41,34 +37,21 @@ namespace Theogony{
         }
 
         public void ToggleDropdown(){
-            dropdownOpen = !optionHolder.activeSelf;
+            dropdownOpen = !dropdownOpen;
             MenuInfo optionMenu = optionHolder.GetComponent<MenuInfo>();
             if(dropdownOpen){
-                arrowImage.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
                 uIController.OpenMenu(optionHolder);
-                optionMenu.currIndex = currentOption;
+                optionMenu.currIndex = optionIndex;
             }else{
-                arrowImage.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
                 uIController.OpenMenu(optionMenu.previousMenu);
             }
         }
 
         public void UpdateOption(int newIndex){
-            previousIndex = -1;
-            currentOption = newIndex;
-            optionString = options[currentOption].gameObject.name;
+            optionIndex = newIndex;
+            optionString = options[optionIndex].gameObject.name;
             optionText.text = optionString;
             ToggleDropdown();
         }
-
-        public bool ValueChanged(){
-            bool changed = (previousIndex != currentOption);
-            previousIndex = currentOption;
-            return changed;
-        }
     }
-
-
-
-    
 }
--- Assets/+Scripts/UI/MenuInfo.cs
+++ Assets/+Scripts/UI/MenuInfo.cs
@@ -2,19 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor;
 
-namespace Theogony{
-    public class MenuInfo : MonoBehaviour
-    {
-        public GameObject previousMenu;
-        [Tooltip("If the previous menu is closed when this one is opened")]
-        public bool closesPrevious;
-        public bool saveIndex;
-        public Button[] buttons;
-        public int rowSize;
-        public int currIndex;
-        public Sprite[] buttonSprites;
-        public Color[] buttonTextColors;
-    }
+public class MenuInfo : MonoBehaviour
+{
+    public GameObject previousMenu;
+    public bool closesPrevious;
+    public bool saveIndex;
+    public Button[] buttons;
+    public int rowSize;
+    public int currIndex;
+    public Sprite[] buttonSprites;
+    public Color[] buttonTextColors;
 }
